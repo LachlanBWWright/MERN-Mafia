31cdbbfcb9df670ba3d8cd3bbfc093295843d39f
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-09-13 21:33:46 +1000

    Refactoring, changed voting method

--- model/rooms/room.js
+++ model/rooms/room.js
@@ -60,7 +60,7 @@ class Room {
 
         this.io.to(this.name).emit('receive-message', (playerUsername + ' has joined the room!'));
         this.playerList.push(new Player(playerSocketId, playerUsername)); //Adds a player to the array
-        this.playerCount = Object.keys(this.playerList).length; //Updates the player count
+        this.playerCount = this.playerList.length; //Updates the player count
 
         let playerObject = {};
         playerObject.name = playerUsername            
@@ -71,9 +71,7 @@ class Room {
             this.started = true;
             this.io.to(this.name).emit('receive-message', 'The room is full! Starting the game!');
             this.emitPlayerList(this.name);
-            this.playerList.forEach(player => {
-                this.gameDB.players.push({playerName: player.playerUsername})
-            })
+            this.playerList.forEach(player => this.gameDB.players.push({playerName: player.playerUsername}));
             this.startGame();
         }
         return playerUsername; //Successfully joined
@@ -81,16 +79,14 @@ class Room {
 
     //Handles a player being removed if they've disconnected
     removePlayer(playerSocketId) {
-        console.log('Disconnect Test');
         for(let i = 0; i < this.playerList.length; i++) {
             if(this.playerList[i].socketId === playerSocketId && !this.gameHasEnded) { //!gameHasEnded stops leaving messages when the clients are booted when the game ends
                 if(!this.started) { //Removes the player if the game has not started
                     let playerObject = {};
                     playerObject.name = this.playerList[i].playerUsername;            
                     this.io.to(this.name).emit('remove-player', playerObject);
-                    
                     this.io.to(this.name).emit('receive-message', (this.playerList[i].playerUsername + ' has left the room!'))
-                    this.playerList.splice(i, 1); //This should remove the player from the array
+                    this.playerList.splice(i, 1);
                     this.playerCount = Object.keys(this.playerList).length;
                 }
                 else { //Kills the player if the game has started
@@ -102,25 +98,17 @@ class Room {
     }
 
     isInRoom(playerSocketId) {
-        for(let i = 0; i < this.playerList.length; i++) {
-            if(this.playerList[i].socketId == playerSocketId)
-                return true;
-        }
+        for(let i = 0; i < this.playerList.length; i++) if(this.playerList[i].socketId == playerSocketId) return true;
         return false;
     }
 
     getPlayerByUsername(playerUsername) {
-        for(let i = 0; i < this.playerList.length; i++) {
-            if(this.playerList[i].playerUsername.toLowerCase() == playerUsername) {
-                return this.playerList[i];
-            }      
-        }
+        for(let i = 0; i < this.playerList.length; i++) if(this.playerList[i].playerUsername.toLowerCase() == playerUsername) return this.playerList[i];
         return false;
     }
 
     emitPlayerList(socketId) { //Returns a player list 
         let playersReturned = [];
-
         for(let i = 0; i < this.playerList.length; i++) {
             let playerReturned = {};
             playerReturned.name = this.playerList[i].playerUsername;
@@ -128,7 +116,6 @@ class Room {
             if(this.started && !this.playerList[i].isAlive) this.playerList[i].role.name; //Reveals the role if the player is dead, and the game has started
             playersReturned.push(playerReturned);
         }
-
         this.io.to(socketId).emit('receive-player-list', playersReturned);
     }
 
@@ -140,11 +127,9 @@ class Room {
                 if(foundPlayer.isAlive) {
                     //Starts with a / - Whisper/Role Command
                     if(message.charAt(0) == '/') {
-                        if(message.charAt(1) == 'w' || message.charAt(1) == 'W') { //Handle whispering
-                            foundPlayer.role.handlePrivateMessage(message);
-                        }
+                        if(message.charAt(1) == 'w' || message.charAt(1) == 'W') foundPlayer.role.handlePrivateMessage(message); //Handle whispering
                         else if((message.charAt(1) == 'c' || message.charAt(1) == 'C') && this.time == 'day') { //Handle daytime commands
-                            if(message.length == 2)  foundPlayer.role.cancelDayAction();
+                            if(message.length == 2) foundPlayer.role.cancelDayAction();
                             else foundPlayer.role.handleDayAction(message);
                         }
                         else if((message.charAt(1) == 'c' || message.charAt(1) == 'C') && this.time == 'night') { //Handle nighttime commands
@@ -152,34 +137,19 @@ class Room {
                                 if(message.length == 2)  foundPlayer.role.cancelNightAction();
                                 else foundPlayer.role.handleNightAction(message);
                             }
-                            else { //Generally, the only time when a player is roleblocked before the end of night is when they are jailed
-                                this.io.to(playerSocketId).emit('receive-message', 'You are roleblocked, and cannot call commands.');
-                            }
+                            else this.io.to(playerSocketId).emit('receive-message', 'You are roleblocked, and cannot call commands.'); //Generally, the only time when a player is roleblocked before the end of night is when they are jailed
                         }
-                        else if((message.charAt(1) == 'v' || message.charAt(1) == 'V') && this.time != 'day') {
-                            this.io.to(playerSocketId).emit('receive-message', 'You cannot vote at night.');
-                        }
-                        else if((message.charAt(1) == 'v' || message.charAt(1) == 'V') && this.time == 'day') {
-                            this.handleVote(message, foundPlayer);
-                        }
-                    
+                        else if((message.charAt(1) == 'v' || message.charAt(1) == 'V') && this.time != 'day') this.io.to(playerSocketId).emit('receive-message', 'You cannot vote at night.');
+                        else if((message.charAt(1) == 'v' || message.charAt(1) == 'V') && this.time == 'day') this.handleVote(message, foundPlayer);
                     }
-                    //Doesn't start with either - send as a regular message
-                    else {
-                       foundPlayer.role.handleMessage(message);
-                    }
+                    else foundPlayer.role.handleMessage(message); //Doesn't start with either - send as a regular message
                 }
-                else {
-                    this.io.to(playerSocketId).emit('receive-message', 'You cannot speak, as you are dead.');
-                }
+                else this.io.to(playerSocketId).emit('receive-message', 'You cannot speak, as you are dead.');
             }
-            else { //If the game hasn't started, no roles have been assigned, just send the message directly
-                this.io.to(this.name).emit('receive-chat-message', (foundPlayer.playerUsername + ': ' + message));
-            }
+            else this.io.to(this.name).emit('receive-chat-message', (foundPlayer.playerUsername + ': ' + message));  //If the game hasn't started, no roles have been assigned, just send the message directly
         }
         catch (error) {
-            this.io.to(playerSocketId).emit('receive-message', 'You cannot speak, as you are dead. Or an error occured.'); //Error is thrown if a player cannot be found
-            console.log(error);
+            this.io.to(playerSocketId).emit('receive-message', 'You cannot speak, as you are dead. Or an error occured.'); console.log(error);//Error is thrown if a player cannot be found
         }
     }
 
@@ -263,8 +233,9 @@ class Room {
         let livingPlayerList = [];
         for(let i = 0; i < this.playerList.length; i++) {
             if(this.playerList[i].isAlive) {
+                this.playerList[i].hasVoted = false;
+                this.playerList[i].votesReceived = 0;
                 livingPlayerList.push(this.playerList[i]);
-                livingPlayerList.votingFor = null; //Resets votes
             }
         }
         
@@ -273,25 +244,13 @@ class Room {
 
         setTimeout(() => {
             try {
-                //Eliminates the player if they have a majority of the votes.
-                for(let i = 0; i < livingPlayerList.length; i++) {
-                    let votesForPlayer = 0;
-                    for(let x = 0; x < livingPlayerList.length; x++) {
-                        if(livingPlayerList[x].votingFor == livingPlayerList[i]) {
-                            votesForPlayer++;
-                        }
-                    }
-                    if(votesForPlayer >= votesRequired) {
+                for(let i = 0; i < livingPlayerList.length; i++) { //Eliminates the player if they have a majority of the votes.
+                    if(livingPlayerList[i].votesReceived >= votesRequired) {
                         this.io.to(this.name).emit('receive-message', (livingPlayerList[i].playerUsername + ' has been voted out by the town.'));
                         this.io.to(livingPlayerList[i].socketId).emit('receive-message', 'You have been voted out of the town.');
                         this.io.to(livingPlayerList[i].socketId).emit('block-messages');
                         livingPlayerList[i].isAlive = false;
-
-                        let tempPlayer = {};
-                        tempPlayer.name = livingPlayerList[i].playerUsername;
-                        this.io.to(this.name).emit('update-player-role', tempPlayer);
-
-
+                        this.io.to(this.name).emit('update-player-role', {name: livingPlayerList[i].playerUsername}); //Marks player as dead client-side, does not reveal their role
                     }
                 }
 
@@ -345,12 +304,7 @@ class Room {
                 }
 
                 //Roleblocking classes go first, and give the victims the roleblocked attribute
-                for(let i = 0; i < this.playerList.length; i++) {
-                    if(this.playerList[i].role.roleblocker) {
-                        this.playerList[i].role.visit();
-                    }
-                }
-   
+                for(let i = 0; i < this.playerList.length; i++) if(this.playerList[i].role.roleblocker) this.playerList[i].role.visit();
                 //Marks who has visited who, and handles players whose abilities were disabled by being roleblocked
                 for(let i = 0; i < this.playerList.length; i++) {
                     if(this.playerList[i].role.roleblocked && !this.playerList[i].role.roleblocker) { //Cancels vists for players that were roleblocked, and informs them.
@@ -358,18 +312,10 @@ class Room {
                         this.io.to(this.playerList[i].socketId).emit('receive-message', 'You were roleblocked!');
                         this.playerList[i].role.roleblocked = false;
                     }
-                    else if(this.playerList[i].role.visiting != null) {
-                        this.playerList[i].role.visit();
-                    }
+                    else if(this.playerList[i].role.visiting != null) this.playerList[i].role.visit();
                 }
-                
                 //Executes the effects that each visit has
-                for(let i = 0; i < this.playerList.length; i++) {
-                    if(this.playerList[i].isAlive) {
-                        this.playerList[i].role.handleVisits(); //Handles actions for certain roles whose behaviour depends on who has visited who.
-                    }
-                }
-
+                for(let i = 0; i < this.playerList.length; i++) if(this.playerList[i].isAlive) this.playerList[i].role.handleVisits(); //Handles actions for certain roles whose behaviour depends on who has visited who.
                 //Kills players who have been attacked without an adequate defence, resets visits after night logic has been completed
                 for(let i = 0; i < this.playerList.length; i++) {
                     if(this.playerList[i].isAlive) {
@@ -381,18 +327,13 @@ class Room {
                         this.playerList[i].role.nightTapped = false; 
                     }
                 }
-                
             }
             catch(error) { //If something goes wrong, just start the next period of time
                 console.log(error);
             }
 
-            if(this.findWinningFaction() != false) {
-                this.endGame(this.findWinningFaction());
-            }
-            else {
-                this.startDaySession(nightNumber + 1, sessionLength);
-            }  
+            if(this.findWinningFaction() != false) this.endGame(this.findWinningFaction());
+            else this.startDaySession(nightNumber + 1, sessionLength);
         }, 15000); //Nights are 15 seconds long.
         
     }
@@ -404,20 +345,13 @@ class Room {
         let recipient = this.getPlayerByUsername(messageRecipientName);
         message = message.substring(messageRecipientName.length).trim(); //Removes the name, trying to leave just the message
     
-        if(this.time == 'day' && recipient.isAlive) {
-            this.io.to(this.name).emit('receive-message', (voterPlayer.playerUsername + ' has voted for ' + recipient.playerUsername + ' to be executed!'));
-            voterPlayer.votingFor = recipient; //Casts the vote
-
-            let votesForPlayer = 0; //Iterate through playerList, and find out how many people have voted for them
-            for(let i = 0; i < this.playerList.length; i++) if(this.playerList[i].isAlive && this.playerList[i].votingFor == recipient) votesForPlayer++;
-            if(votesForPlayer > 1) this.io.to(this.name).emit('receive-message', ('There are ' + votesForPlayer + ' votes for ' + recipient.playerUsername + ' to be killed.'));
-            else this.io.to(this.name).emit('receive-message', ('There is one vote for ' + recipient.playerUsername + ' to be killed.'));
+        if(this.time == 'day' && recipient.isAlive && !voterPlayer.hasVoted) {
+            voterPlayer.hasVoted = true;
+            recipient.votesReceived++;
+            if(recipient.votesReceived > 1) this.io.to(this.name).emit('receive-message', (voterPlayer.playerUsername + ' has voted for ' + recipient.playerUsername + ' to be executed! There are ' + recipient.votesReceived + ' votes for ' + recipient.playerUsername + ' to be killed.'));
+            else this.io.to(this.name).emit('receive-message', (voterPlayer.playerUsername + ' has voted for ' + recipient.playerUsername + ' to be executed! There is one vote for ' + recipient.playerUsername + ' to be killed.'));
         }
-        else if(message == '' && voterPlayer.votingFor != null) {
-            this.io.to(voterPlayer.socketId).emit('receive-message', 'Your vote has been rescinded.');
-            this.io.to(this.name).emit('receive-message', (voterPlayer.playerUsername + ' has cancelled their vote.'));
-            voterPlayer.votingFor = null;
-        }
+        else if(voterPlayer.hasVoted) this.io.to(voterPlayer.socketId).emit('receive-message', 'You cannot change your vote.');
         else this.io.to(voterPlayer.socketId).emit('receive-message', 'Your vote was invalid.');
     }
 
