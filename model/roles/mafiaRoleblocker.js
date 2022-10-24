06f7695ae445be5be2b8be607a73895a929c51e6
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-10-24 23:44:30 +1100

    Development for new roles

---
+++ model/roles/mafiaRoleblocker.js
@@ -0,0 +1,47 @@
+import Role from './role.js'
+
+class MafiaRoleblocker extends Role {
+    constructor(room, player) {
+        super('MafiaRoleblocker', 'mafia', room, player, 0, false, false, false, false, false, false, false, true);
+        this.attackVote;
+    }
+
+    handleNightVote(recipient) {
+        this.attackVote = recipient
+        if(this.attackVote.playerUsername != undefined && this.attackVote.role.faction != this.faction && this.attackVote.isAlive) {
+            this.faction.sendMessage(this.player.playerUsername + ' has voted to attack ' + this.attackVote.playerUsername + '.');
+            this.attackVote = this.attackVote.role; //uses role for easier visiting
+        }
+        else {
+            this.room.io.to(this.player.socketId).emit('receive-message', 'Invalid Vote.');
+        }
+    }
+
+    handleNightAction(recipient) { //Vote on who should be attacked
+        if(recipient == this.player) {
+            this.room.io.to(this.player.socketId).emit('receive-message', 'You cannot block yourself.');
+        }
+        else if(recipient.playerUsername != undefined && recipient.isAlive) {
+            this.room.io.to(this.player.socketId).emit('receive-message', 'You have chosen to block ' + recipient.playerUsername + '.');
+            this.visiting = recipient.role;
+        }
+        else {
+            this.room.io.to(this.player.socketId).emit('receive-message', 'Invalid choice.');
+        }
+    }
+
+    cancelNightAction() { //Faction-based classes should override this function
+        this.room.io.to(this.player.socketId).emit('receive-message', 'You have cancelled your class\' nighttime action.');
+        this.attackVote = null;
+    }
+
+    visit() { //This visits a role and attacks them. this.visiting is dictated by the faction Class.
+        if(this.visiting != null) {
+            this.visiting.receiveVisit(this);
+            if(this.visiting.damage == 0) this.visiting.damage = 1; //Attacks the victim
+            this.visiting.attackers.push(this);
+        }
+    }
+}
+
+export default MafiaRoleblocker;
